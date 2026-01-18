838e1f05c5fc4d443eeca53fceb52ac2dcaa285c
VicChen199 <95987178+VicChen199@users.noreply.github.com>
2026-01-18 14:58:41 -0500

    Generate pie charts
    Creates pass/fail charts for a specified category in its own individual window

--- runner.py
+++ runner.py
@@ -1,5 +1,11 @@
+''' 
+TODO: Add graphs using matplotlib for pie charts of:
+    pass/fail of exam average
+    pass/fail of HW average 
+'''
 import pandas as pd
-import os
+#import os
+import matplotlib.pyplot as plt
 
 # This function cleans data for homeworks and exams
 #   Can be used for other similar formats like CW grades
@@ -35,13 +41,16 @@ def cleanAssignmentDataFrame(dataframe: pd.DataFrame):
 # Calculates the averages for each assignment type to each student
 def calculateAverageAssignment(dataframe: pd.DataFrame, type):
 
-    # Gets a list of the assignment/column names
-    assignmentNames = dataframe.columns.tolist()
-    assignmentNames.pop(0)
-
     # Initializes necessary variables
     column = type + " Average"
     averages = []
+
+    # Remove the averages column if it already exists since it has to be updated
+    dataframe = dataframe.drop(column, axis = 1, errors = "ignore")
+
+    # Gets a list of the assignment/column names
+    assignmentNames = dataframe.columns.tolist()
+    assignmentNames.pop(0)
 
     # Takes the average of each student's scores
     for student in dataframe.index:
@@ -54,6 +63,30 @@ def calculateAverageAssignment(dataframe: pd.DataFrame, type):
 
     # Adds a column to the dataframe for the avergaes
     dataframe[column] = averages
+    dataframe[column] = dataframe[column].astype(int)
+
+    # Returns the changed dataframe
+    return dataframe
+
+def generatePassFailPieChart(column, position):
+    # Gets the number of students that passed and failed the class
+    passCount = 0
+    failCount = 0
+    for student in master.index:
+        if master.loc[student, column] >= 65:
+            passCount += 1
+        else:
+            failCount += 1
+
+    # Plots Pass/Fail Ratio Pie Chart for the class
+    passFrame = pd.DataFrame({
+        "State": ["Pass", "Fail"],
+        "Count": [passCount, failCount]
+    })
+    passFrame = passFrame.set_index("State")
+    plt.figure(position)
+    passFrame.plot.pie(title = column, y = "Count", figsize = (5,5))
+    plt.axis("equal")
 
 # Prepares the general gradebook, which will only consist of Name, Final Average, Exam Pass Rate, Homework Pass Rate
 with open("General_Gradebook.csv", "w") as f:
@@ -73,12 +106,11 @@ policy = pd.read_csv("Grading_Policy.csv")
 # Cleans the data from homeworks and exams
 cleanAssignmentDataFrame(homeworks)
 cleanAssignmentDataFrame(exams)
-calculateAverageAssignment(homeworks, "HW")
-calculateAverageAssignment(exams, "Exam")
+homeworks = calculateAverageAssignment(homeworks, "HW")
+exams = calculateAverageAssignment(exams, "Exam")
 
 # Cleans the grading policy information
 
-# TODO: perhaps create a separate function for this section of cleaning
 # Gets a list of the assignment/column names
 policyColumns = policy.columns.tolist()
 
@@ -94,8 +126,6 @@ homeworkAndExams = pd.merge(exams, homeworks, on="Name", how="outer")
 master["Name"] = homeworkAndExams["Name"]
 master = pd.merge(master, homeworkAndExams, on="Name", how="left")
 
-print(master)
-
 # Receives relevant grading policy information
 examWeight = policy.loc[0, "Exam Weight"]
 homeWeight = policy.loc[0, "Homework Weight"]
@@ -103,15 +133,27 @@ totalWeight = examWeight + homeWeight
 examWeight /= totalWeight
 homeWeight /= totalWeight
 
+# Adds the final averages to the master dataframe
 finalAvgs = []
 for studentNum in master.index:
     finalAvgs.append(examWeight * master.loc[studentNum, "Exam Average"] + homeWeight * master.loc[studentNum, "HW Average"])
 master["Final Average"] = finalAvgs
+master["Final Average"] = master["Final Average"].astype(int)
 
-print(master)
+# Populates the general gradebook
+gradebook["Name"] = master["Name"]
+gradebook["Final Average"] = master["Final Average"]
+gradebook["Exam Average"] = master["Exam Average"]
+gradebook["HW Average"] = master["HW Average"]
 
 # Execution of code, aka testing for now
 policy.to_csv("Grading_Policy.csv", index = False)
 master.to_csv("Master_Gradebook.csv", index = False)
+gradebook.to_csv("General_Gradebook.csv", index = False)
 homeworks.to_csv("Homework_Grades.csv", index = False)
 exams.to_csv("Exam_Grades.csv", index = False)
+
+generatePassFailPieChart("Final Average", 1)
+generatePassFailPieChart("Exam Average", 2)
+generatePassFailPieChart("HW Average", 3)
+plt.show()
